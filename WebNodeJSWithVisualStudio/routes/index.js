c24221da989b1b00cacd6a3e0b47aea0db74c0b6
IsraelPuyol <israelpuyol@hotmail.com>
2017-06-06 14:55:33 +0200

    tamaño letra y color texto cabecera

--- WebNodeJSWithVisualStudio/routes/index.js
+++ WebNodeJSWithVisualStudio/routes/index.js
@@ -34,7 +34,12 @@ exports.UsersList = function (req, res) {
 
 exports.InsertUser = function (req, res) {
 
-    res.render('InsertUser', { year: new Date().getFullYear(), message: 'INTRODUCE UN NUEVO USUARIO' , messageNombreNull: '', messageApellido1Null: '', messageApellido2Null: ''});
+    res.render('InsertUser', {
+        year: new Date().getFullYear(), message: 'INTRODUCE UN NUEVO USUARIO' , 
+        messageNombreNull: '', 
+        messageApellido1Null: '', 
+        messageApellido2Null: ''
+    });
     
    
 };
